fabf0d3163ce309026068b45bcc12d00190819ee
Zhang, Lili Z <lili.z.zhang@intel.com>
2023-08-21 18:02:05 +0800

    [from DCAP] Intel(R) SGX DCAP 1.18 Release
    Introduced Intel(R) TDX 1.4 and 1.5 support
    Upgraded Ring3 Abstraction Layer (R3AAL) library to support
    Intel(R) TDX MVP 6.2 kernel
    Enhanced quote verification performance in multi-thread scenarios
    Upgraded Intel(R) SGX Quote Verification Enclave to integrate
    latest OpenSSL/SgxSSL 1.1.1u
    Fixed bugs.
    
    Signed-off-by: Zhang, Lili Z <lili.z.zhang@intel.com>

--- QuoteGeneration/quote_wrapper/common/inc/sgx_quote_5.h
+++ QuoteGeneration/quote_wrapper/common/inc/sgx_quote_5.h
@@ -30,24 +30,23 @@
  */
 
 /**
- * File: sgx_quote_4.h
+ * File: sgx_quote_5.h
  * Description: Definition for quote structure.
  *
  * Quote structure and all relative structure will be defined in this file.
  */
 
-#ifndef _SGX_QUOTE_4_H_
-#define _SGX_QUOTE_4_H_
+#ifndef _SGX_QUOTE_5_H_
+#define _SGX_QUOTE_5_H_
 
-#include "sgx_quote_3.h"
-#include "sgx_report2.h"
-#include "sgx_quote.h"
+#include "sgx_quote_4.h"
 
 
 #pragma pack(push, 1)
 
-#define TD_INFO_RESERVED_BYTES_V1 112
-typedef struct _tee_info_t                    /* 512 bytes */
+#define QE_QUOTE_VERSION_V5 5
+#define TD_INFO_RESERVED_BYTES_V1_5 64
+typedef struct _tee_info_v1_5_t                 /* 512 bytes */
 {
     tee_attributes_t     attributes;          /* (  0) TD's attributes */
     tee_attributes_t     xfam;                /* (  8) TD's XFAM */
@@ -56,74 +55,37 @@ typedef struct _tee_info_t                    /* 512 bytes */
     tee_measurement_t    mr_owner;            /* (112) Software defined ID for the guest TD's owner */
     tee_measurement_t    mr_owner_config;     /* (160) Software defined ID for owner-defined configuration of the guest TD, e.g., specific to the workload rather than the runtime or OS */
     tee_measurement_t    rt_mr[4];            /* (208) Array of 4(TDX1: NUM_RTMRS is 4) runtime extendable measurement registers */
-    uint8_t reserved[TD_INFO_RESERVED_BYTES_V1]; /* (400) Reserved, must be zero */
-} tee_info_t;
+    tee_measurement_t    mr_servicetd;        /* (400) If is one or more bound or pre-bound service TDs, SERVTD_HASH is the SHA384 hash of the TDINFO_STRUCTs of those service TDs bound.
+                                                       Else, SERVTD_HASH is 0. */
+    uint8_t reserved[TD_INFO_RESERVED_BYTES_V1_5]; /* (448) Reserved, must be zero */
+} tee_info_v1_5_t;
 
 
-#define TEE_TCB_SVN_SIZE        16
-typedef struct _tee_tcb_svn_t
+#define TD_TEE_TCB_INFO_RESERVED_BYTES_V1_5 95
+typedef struct _tee_tcb_info_v1_5_t
 {
-    uint8_t  tcb_svn[TEE_TCB_SVN_SIZE];
-} tee_tcb_svn_t;
-
-#define TD_TEE_TCB_INFO_RESERVED_BYTES_V1 111
-typedef struct _tee_tcb_info_t
-{
-    uint8_t           valid[8];                                   /* (  0) Indicates TEE_TCB_INFO fields which are valid
-                                                                           - 1 in the i-th significant bit reflects that the field starting at byte offset(8*i)
-                                                                           - 0 in the i-th significant bit reflects that either no field start by byte offset(8*i) or that
-                                                                               field is not populated and is set to zero. */
+    uint8_t           valid[8];                                   /* (  0) Indicates TEE_TCB_INFO fields which are valid */
+                                                                  /*       - 1 in the i-th significant bit reflects that the field starting at byte offset(8*i) */
+                                                                  /*       - 0 in the i-th significant bit reflects that either no field start by byte offset(8*i) or that */
+                                                                  /*           field is not populated and is set to zero. */
+                                                                  /*       the accepted value of a TDX 1.5 tee_tcb_info_v2 is 0x013ff. (Note: Set to 0x301FF if */
+                                                                  /*       SEAMDB_ENABLED == ‘1, otherwise set to 0x1FF. (SEAMDB_ENABLED is introduced for TDX1.4 TD Preserving)*/
     tee_tcb_svn_t     tee_tcb_svn;                                /* (  8) TEE_TCB_SVN Array */
     tee_measurement_t mr_seam;                                    /* ( 24) Measurement of the SEAM module */
     tee_measurement_t mr_seam_signer;                             /* ( 72) Measurement of SEAM module signer. (Not populated for Intel SEAM modules) */
     tee_attributes_t  attributes;                                 /* (120) Additional configuration attributes.(Not populated for Intel SEAM modules) */
-    uint8_t           reserved[TD_TEE_TCB_INFO_RESERVED_BYTES_V1];/* (128) Reserved, must be zero */
-} tee_tcb_info_t;
-
-/** The SGX_QL_SGX_QL_ALG_ECDSA_P256 specific data structure.  Appears in the signature_data[] of the sgx_quote3_t
- *  structure. */
-typedef struct _sgx_qe_report_certification_data_t {
-     sgx_report_body_t   qe_report;           ///< QE Report of the QE when the Att key was generated.  The ReportData will contain the ECDSA_ID
-     uint8_t             qe_report_sig[32*2]; ///< Signature of QE Report using the Certification Key (PCK for root signing). Big Endian
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning ( disable:4200 )
-#endif
-     uint8_t             auth_certification_data[];               ///< Place holder for both the auth_data_t and certification_data_t.  Concatenated in that order.
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
-} sgx_qe_report_certification_data_t;
-
-typedef struct _sgx_ecdsa_sig_data_v4_t {
-     uint8_t             sig[32*2];            ///< Signature over the Quote using the ECDSA Att key. Big Endian.
-     uint8_t             attest_pub_key[32*2]; ///< ECDSA Att Public Key. Hash in QE Report.ReportData. Big Endian
-#ifdef _MSC_VER
-#pragma warning(push)
-#pragma warning ( disable:4200 )
-#endif
-     uint8_t             certification_data[]; ///< Certification data associated with the cert_key_type
-#ifdef _MSC_VER
-#pragma warning(pop)
-#endif
-} sgx_ecdsa_sig_data_v4_t;
+    tee_tcb_svn_t     tee_tcb_svn2;                               /* (128) Array of TEE TCB SVNs (for TD preserving). */
+    uint8_t           reserved[TD_TEE_TCB_INFO_RESERVED_BYTES_V1_5];/* (144) Reserved, must be zero */
+} tee_tcb_info_v1_5_t;
 
 /** The quote header.  It is designed to compatible with earlier versions of the quote. */
-typedef struct _sgx_quote4_header_t {
-    uint16_t            version;              ///< 0:  The version this quote structure.
-    uint16_t            att_key_type;         ///< 2:  sgx_attestation_algorithm_id_t.  Describes the type of signature in the signature_data[] field.
-    uint32_t            tee_type;             ///< 4:  Type of Trusted Execution Environment for which the Quote has been generated.
-                                              ///      Supported values: 0 (SGX), 0x81(TDX)
-    uint32_t            reserved;             ///< 8:  Reserved field.
-    uint8_t             vendor_id[16];        ///< 12: Unique identifier of QE Vendor.
-    uint8_t             user_data[20];        ///< 28: Custom attestation key owner data.
-} sgx_quote4_header_t;
+typedef sgx_quote4_header_t  sgx_quote5_header_t;
 
-/** SGX Report2 body */
-typedef struct _sgx_report2_body_t {
+/** SGX Report2 body for quote v5 */
+typedef struct _sgx_report2_body_v1_5_t {
     tee_tcb_svn_t       tee_tcb_svn;          ///<  0:  TEE_TCB_SVN Array
     tee_measurement_t   mr_seam;              ///< 16:  Measurement of the SEAM module
-    tee_measurement_t   mrsigner_seam;        ///< 64:  Measurement of a 3rd party SEAM module’s signer (SHA384 hash). 
+    tee_measurement_t   mrsigner_seam;        ///< 64:  Measurement of a 3rd party SEAM module’s signer (SHA384 hash).
                                               ///       The value is 0’ed for Intel SEAM module
     tee_attributes_t    seam_attributes;      ///< 112: MBZ: TDX 1.0
     tee_attributes_t    td_attributes;        ///< 120: TD's attributes
@@ -134,26 +96,37 @@ typedef struct _sgx_report2_body_t {
     tee_measurement_t   mr_owner_config;      ///< 280: Software defined ID for owner-defined configuration of the guest TD, e.g., specific to the workload rather than the runtime or OS
     tee_measurement_t   rt_mr[4];             ///< 328: Array of 4(TDX1: NUM_RTMRS is 4) runtime extendable measurement registers
     tee_report_data_t   report_data;          ///< 520: Additional report data
-}sgx_report2_body_t;
+    tee_tcb_svn_t       tee_tcb_svn2;         ///< 584: Array of TEE TCB SVNs (for TD preserving).
+    tee_measurement_t   mr_servicetd;         ///< 600: If is one or more bound or pre-bound service TDs, SERVTD_HASH is the SHA384 hash of the TDINFO_STRUCTs of those service TDs bound.
+                                              ///       Else, SERVTD_HASH is 0..
+}sgx_report2_body_v1_5_t;
 
 /** The generic TD quote data structure.  This is the common part of the quote.  The signature_data[] contains the signature and supporting
  *  information of the key used to sign the quote and the contents depend on the sgx_quote_sign_type_t value. */
-typedef struct _sgx_quote4_t {
-    sgx_quote4_header_t header;               ///< 0:   The quote header.
-    sgx_report2_body_t  report_body;          ///< 48:  The REPORT of the TD that is attesting remotely.
-    uint32_t            signature_data_len;   ///< 632: The length of the signature_data.  Varies depending on the type of sign_type.
+typedef struct _sgx_quote5_t {
+    sgx_quote5_header_t header;               ///< 0:   The quote header.
+    uint16_t type;                            ///< 48: Determines type of Quote body (TEE report)
+                                              ///      Architecturally supported values:
+                                              ///      1 (SGX Enclave Report)
+                                              ///      2 (TD Report for TDX 1.0)
+                                              ///      3 (TD Report for TDX 1.5)
+    uint32_t size;                            ///< 50: Size of Quote Body field.
 #ifdef _MSC_VER
 #pragma warning(push)
-#pragma warning ( disable:4200 )
+#pragma warning(disable : 4200)
 #endif
-    uint8_t             signature_data[];     ///< 636: Contains the variable length containing the quote signature and support data for the signature.
+    uint8_t body[]; ///< 54: Data conveyed as Quote Body. Its content depends on the value of Quote Body Type
+                    ///     1 Byte array that contains SGX Enclave Report.
+                    ///       sgx_report_body_t + (uint32_t)signature_data_len + signature
+                    ///     2 Byte array that contains TD Report for TDX 1.0.
+                    ///       sgx_report2_body_t + (uint32_t)signature_data_len + signature
+                    ///     3 Byte array that contains TD Report for TDX 1.5.
+                    ///       sgx_report2_body_v1_5_t + (uint32_t)signature_data_len + signature
 #ifdef _MSC_VER
 #pragma warning(pop)
 #endif
-} sgx_quote4_t;
-
-typedef sgx_att_key_id_ext_t tee_att_att_key_id_t;
+} sgx_quote5_t;
 
 #pragma pack(pop)
 
-#endif //_SGX_QUOTE_4_H_
+#endif //_SGX_QUOTE_5_H_
